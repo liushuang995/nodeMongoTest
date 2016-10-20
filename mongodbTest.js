164b52bdfcb763ef83f20b6c391219c49b711380
liushuang995 <842616352@qq.com>
2016-10-20 10:26:23 +0800

    update

--- mongodbTest.js
+++ mongodbTest.js
@@ -16,17 +16,12 @@
 //        });
 //    });
 //});
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017');
-//var db = mongoose.createConnection('mongodb://127.0.0.1:27017');
-//db.on('error',console.error.bind(console,'连接错误:'));
-//db.once('open',function(){
-//    console.info('__')
+
+//mongoose.connect('mongodb://127.0.0.1:27017');
+//var PersonSchema = new mongoose.Schema({
+//    name:String ,  //定义一个属性name，类型为String
+//    like:String
 //});
-var PersonSchema = new mongoose.Schema({
-    name:String ,  //定义一个属性name，类型为String
-    like:String
-});
 //var PersonModel = db.model('Person',PersonSchema);
 //var personEntity = new PersonModel({name:'liushaung995'});
 //console.log(personEntity.name)
@@ -36,52 +31,65 @@ var PersonSchema = new mongoose.Schema({
 //    console.info(persons)
 //});
 //var Cat = mongoose.model('Catsss', { name: String,id:String,age:Number });
-PersonSchema.add({age:Number})
-var Cat = mongoose.model('Catsss', PersonSchema);
-var del  = {name:"liushuang123456"};
-//删除数据
-//Cat.remove(del,function(err,result){
+//PersonSchema.add({age:Number})
+//var Cat = mongoose.model('Catsss', PersonSchema);
+//var del  = {name:"liushuang123456"};
+////删除数据
+////Cat.remove(del,function(err,result){
+////    if(err){
+////        console.log(err);
+////    }else{
+////        console.log("update");
+////    }
+////});
+//// 原数据字段值
+//var oldValue  = {age:20};
+//// 单条件更新
+//var newData1 = {$set:{name:"内容"}};
+//// 多条件更新
+//var newData2 = {$set:{name:"内容",age:2}};
+//Cat.update(oldValue,newData2,function(err,result){
 //    if(err){
 //        console.log(err);
 //    }else{
 //        console.log("update");
 //    }
 //});
-// 原数据字段值
-var oldValue  = {age:20};
-// 单条件更新
-var newData1 = {$set:{name:"内容"}};
-// 多条件更新
-var newData2 = {$set:{name:"内容",age:2}};
-Cat.update(oldValue,newData2,function(err,result){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("update");
-    }
-
-});
 
 //插入数据
 // 连接数据库
-var db = mongoose.createConnection('mongodb://127.0.0.1:27017/test');
+//var db = mongoose.createConnection('mongodb://127.0.0.1:27017/mydb');
 // 设置数据类型
 var monSchema = new mongoose.Schema({
     name:{type:String,default:"username"},
     age:{type:Number},
     sex:{type:String}
 });
-// 选择集合
+//// 选择集合
+//var monModel = db.model('user',monSchema);
+//// 数据集
+//var content = {name:"Nick",age:23,sex:'男'};
+////// 实例化对象并插入数据
+//var monInsert = new monModel(content);
+//monInsert.save(function(err){
+//    if(err){
+//        console.log(err);
+//    }else{
+//        console.log('成功插入数据');
+//    }
+//    db.close();
+//});
+
+//查询
+ //选择集合
 var monModel = db.model('user',monSchema);
-// 数据集
-var content = {name:"Nick",age:23,sex:'男'};
-// 实例化对象并插入数据
-var monInsert = new monModel(content);
-monInsert.save(function(err){
+var content = {name:"Nick"};
+var field = {name:1,age:1,sex:1};
+monModel.find(content,field,function(err,result){
     if(err){
         console.log(err);
     }else{
-        console.log('成功插入数据');
+        console.log(result);
     }
     db.close();
 });
